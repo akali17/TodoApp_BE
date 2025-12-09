74bcb670acf805c291a4f302e88fa13ecb24fe90
akali17 <2251120334@ut.edu.vn>
2025-12-09 13:19:09 +0900

    base things

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const cors = require("cors")
+const routes = require("./routes");
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use("/api", routes);
+
+app.get("/", (req, res) => {
+    res.json({ message: "Backend running!!" })
+})
+
+module.exports = app
