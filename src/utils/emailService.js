ad107fdb7b00af89c1e0181b27b26c60aac1dc62
akali17 <2251120334@ut.edu.vn>
2025-12-29 03:47:53 +0900

    fix: add SMTP timeout to prevent long connection attempts

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -9,6 +9,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 5000,
+  socketTimeout: 10000,
 });
 
 // Test connection
